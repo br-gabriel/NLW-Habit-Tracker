268e27a2b35bbfc6ba434831788b5a28aa3f4f63
Gabriel <contato.gabrielfeitosa@gmail.com>
2023-01-18 18:33:07 -0300

    added new components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,14 @@
-function App() {
+import './styles/global.css'
+import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
+
+export function App() {
   return (
-    <h1>Hello World</h1>
+    <div className='bg-background text-white w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
   )
 }
-
-export default App
