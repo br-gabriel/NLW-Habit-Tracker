31111b1be3eeb6bd7a640969757289dd37255842
Gabriel <contato.gabrielfeitosa@gmail.com>
2023-01-22 19:37:11 -0300

    Added checkbox to select the days of the week

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,6 +1,17 @@
 import { Check } from "phosphor-react";
+import * as Checkbox from '@radix-ui/react-checkbox';
 
-export function NewHabitForm() {
+const avaibleWeekDays = [
+    'Domingo',
+    'Segunda-feira',
+    'Terça-feira',
+    'Quarta-feira',
+    'Quinta-feira',
+    'Sexta-feira',
+    'Sábado'
+]
+
+export function NewHabitForm() { 
     return (
         <form className="w-full flex flex-col mt-6">
             <label htmlFor="title" className="font-semibold leading-tight">
@@ -18,6 +29,28 @@ export function NewHabitForm() {
             <label htmlFor="" className="font-semibold leading-tight mt-4">
                 Qual a recorrência?
             </label>
+
+            <div className='mt-3 flex flex-col gap-2'>
+                {avaibleWeekDays.map(weekDay => {
+                    return (
+                        <Checkbox.Root
+                        key={weekDay}
+                            className='flex items-center gap-3 group'
+                        >
+                            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+                                <Checkbox.Indicator>
+                                    <Check size={20} className='text-white' />
+                                </Checkbox.Indicator>
+                            </div>
+
+                            <span className='text-white leading-tight'>
+                                {weekDay}
+                            </span>
+                        </Checkbox.Root>
+                    )
+                })}
+                        
+            </div>
 
             <button type="submit" className="mt-6 rounded-lg p-4 flex items-center gap-3 justify-center font-semibold bg-green-600 hover:bg-green-500">
                 <Check size={20} weight="bold" />
