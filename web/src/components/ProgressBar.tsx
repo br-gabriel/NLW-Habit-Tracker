caad766bc8fdd4bea3f0ab62a1e8b4a770718176
Gabriel <contato.gabrielfeitosa@gmail.com>
2023-01-23 18:33:14 -0300

    added transition

--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -10,7 +10,7 @@ export function ProgressBar(props: ProgressBarProps) {
                 aria-label="Progresso de hábitos completados nesse dia"
                 aria-valuemin={0}
                 aria-valuenow={props.progress}
-                className='h-3 rounded-xl bg-violet-600'
+                className='h-3 rounded-xl bg-violet-600 transition-all'
                 style={{width: `${props.progress}%`}}    
             />
         </div>
