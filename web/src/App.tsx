2cd4bf6ab17dd0912a7026fcf3ef091a12ee1ebd
Gabriel <contato.gabrielfeitosa@gmail.com>
2023-02-02 17:46:28 -0300

    Local notification example

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,6 +3,14 @@ import './lib/daysjs'
 import { Header } from './components/Header'
 import { SummaryTable } from './components/SummaryTable'
 
+// window.Notification.requestPermission(permission => {
+//   if (permission === 'granted') {
+//     new window.Notification('Habits', {
+//       body: 'Texto de exemplo',
+//     })
+//   }
+// })
+
 export function App() {
   return (
     <div className='bg-background text-white w-screen h-screen flex justify-center items-center'>
